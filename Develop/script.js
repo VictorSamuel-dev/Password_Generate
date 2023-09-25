1db10da338f05e1527d3b1b668b17cc59d7e01f2
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2023-09-24 22:46:37 -0400

    added test screenshots of the password generator prompts along with the updated java script code for strings

--- Develop/script.js
+++ Develop/script.js
@@ -9,38 +9,60 @@ var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',];
 
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("Hey! You clicked the button!")
-
+function generatePassword(){
+  var password = "";
+  // console.log("Hey! You clicked the button!")
+}
 // 1. Give the user a prompt for the password critaria
 function getPrompts(){
+    choiceArr = [];
     characterLength = parseInt(prompt("How manu characters do you want your password to be? (8 - 128 characters"));
 
     if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-      
+      alert("Character length has to be a number, 8 - 128 digits. Please try again.");
+      return false;
     }
+
+    if (confirm("Would you like lowercase letters in your password?")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+    if (confirm("Would you like uppercase letters in your password?")) {
+      choiceArr = choiceArr.concat(upperCaseArr);
+    }
+    if (confirm("Would you like special characters in your password?")) {
+      choiceArr = choiceArr.concat(specialCharArr);
+    }
+    if (confirm("Would you like numbers in your password?")) {
+      choiceArr = choiceArr.concat(numberArr);
+    }
+    return true;
 }
-//    a. password length between 8 < 128
-//    b. lowercase, uppercase, numbers, special characters,
-const status = prompt("Hello! Create a password between 8 and 128 characters using lowercase, uppercase, numbers, and special characters!"); 
-console.log(status);
-// 2. Validating the input.
+
+// //    a. password length between 8 < 128
+// //    b. lowercase, uppercase, numbers, special characters,
+// const status = prompt("Hello! Create a password between 8 and 128 characters using lowercase, uppercase, numbers, and special characters!"); 
+// console.log(status);
+// // 2. Validating the input.
 
 
-// 3. Generate the password based on criteria
+// // 3. Generate the password based on criteria
 
 
-// 4. Display the password to the page.
-  return "Password I create will go here";
+// // 4. Display the password to the page.
+//   return "Password I create will go here";
 
-}
+// }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+   var  correctPrompts = getPrompts();
 
-  passwordText.value = password;
+  if (correctPrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
 
 }
 
